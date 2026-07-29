b9bd449b3c5f560fd0fc97382479b6bb152e1b0f
khizari <knkhizar@gmail.com>
2026-07-29 11:44:30 +0500

    old project deleted

--- api/session.js
+++
@@ -1,6 +0,0 @@
-const { getSessionFromRequest } = require('../lib/auth');
-
-module.exports = async (req, res) => {
-  const session = getSessionFromRequest(req);
-  res.status(200).json({ authenticated: !!session, username: session ? session.u : null });
-};
